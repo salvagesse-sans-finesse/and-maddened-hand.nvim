0fa88fffd22b64a3426fa4d201247b8480898495
salvagesse-sans-finesse <195239005+salvagesse-sans-finesse@users.noreply.github.com>
2026-06-12 20:10:20 -0600

    Fix formatlistpat value
    The old value was to all apparent purposes working as desired with my
    local nvim 0.10.0, but when I moved to a machine which happened to have
    nvim 0.11.2 installed it no longer matched lines that began with the
    usual "- [ ] " string. The new value gets the desired behaviour out of
    both nvim versions.

--- ftplugin/mh.lua
+++ ftplugin/mh.lua
@@ -1,7 +1,7 @@
 -- The combined effect of the next few settings is to make it so the text after
 -- a checkbox can spill across lines automatically, indenting itself in an
 -- attractive fashion.
-vim.bo.formatlistpat = [[^s*- \[.\] ]]
+vim.bo.formatlistpat = [[^\s*- \[.\] ]]
 vim.bo.formatoptions = 'l'
 vim.wo.breakindent = true
 vim.wo.breakindentopt = "list:6"
